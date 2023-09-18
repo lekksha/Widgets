e4aff18a5612ec3a1c7360506ecc3432f60b3774
lekksha_ <lekksha@gmail.com>
2023-09-18 12:04:41 +0700

    unedited code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <QCoreApplication>
+#include "win.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    Win win(0); //? Почему win(0)
+    win.show();
     return a.exec();
 }
---
+++ win.h
@@ -0,0 +1,32 @@
+#ifndef win_h
+#define win_h
+#include <QtGui>
+class Win:public QWidget // класс окна
+{
+    Q_OBJECT
+protected:
+    QTextCodec *codec;
+    QFrame *frame;
+    QLabel *inputLabel;
+    QLineEdit *outputEdit; // строчный редактор вывода
+    QPushButton *nextButton; // кнопка Следующее
+    QPushButton *exitButton; // кнопка Выход
+public:
+    Win(QWidget *parent = 0); // конструктор
+public slots:
+    void begin(); // метод начальной настройки интерфейса
+    void calc(); // метод реализации вычислений
+};
+
+
+class StrValidator:public QValidator // класс компонента проверки ввода
+{
+public:
+    StrValidator(QObject *parent):QValidator(parent){}
+    virtual State validate(QString &str,int &pos)const
+    {
+        return Acceptable; // метод всегда принимает вводимую строку
+    }
+};
+
+#endif
